13f1fef1e09a3f4ef39453bd3106abb658edfdfe
Tyson Brown <tysonbrown@live.com.au>
2017-03-03 15:40:34 +1000

    Support for debugging in VS Code

--- utilitydelta-frontend/src/app/app.component.ts
+++ utilitydelta-frontend/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UtilityDelta Frontend Hello World!';
+
+  clickNavigation() {
+    this.title = "Navigation clicked";
+  }
 }
