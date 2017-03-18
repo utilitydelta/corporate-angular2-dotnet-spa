f2682d4eff0ced114d74f0f5020e290134697991
Tyson Brown <tysonbrown@live.com.au>
2017-03-18 11:49:29 +1000

    ensure initialApplicationStart state set on home

--- utilitydelta-frontend/src/app/home/home.component.ts
+++ utilitydelta-frontend/src/app/home/home.component.ts
@@ -34,6 +34,7 @@ export class HomeComponent implements OnInit {
           }
           this.currentlyLoading = false;
         });
+      this.currentUserService.initialApplicationStart = false;
     }
   }
 
